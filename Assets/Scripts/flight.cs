e1b82693f504dbd1c5d03f0142facafbf453bc6b
pedropenha <pedro.penha.martins@gmail.com>
2020-10-12 13:38:09 -0300

    sending all

---
+++ Assets/Scripts/flight.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+//using System.Diagnostics;
+//using System.Diagnostics;
+//using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+public class flight : MonoBehaviour
+{
+
+    public float speed;
+    public float rotationSpeed;
+    public float speedUp;
+    public float speedBoost;
+    public GameObject songBoost;
+    public int qtdBirds = 0;
+    public GameObject resetar;
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float translation = 0.5F * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        float sobe = Input.GetAxis("Vertical") * speedUp;
+        float boost = Input.GetAxis("Boost") * speedBoost;
+
+        translation *= Time.deltaTime;
+        boost *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        sobe *= Time.deltaTime;
+
+        if(boost > 0)
+        {
+            songBoost.GetComponent<AudioSource>().volume = 1.0F;
+            transform.Translate(0, 0, translation + boost);
+        }
+        else
+        {
+            songBoost.GetComponent<AudioSource>().volume = 0.0F;
+            transform.Translate(0, 0, translation);
+        }
+
+        
+        transform.Rotate(sobe, rotation, 0);
+
+        if (rotation > 0)
+        {
+            transform.Rotate(0, 0, -2f);
+        }
+        else if (rotation < 0)
+        {
+            transform.Rotate(0, 0, 2f);
+        }
+
+        if(qtdBirds == 4)
+        {
+            resetar.SetActive(true); 
+        }
+    }
+}
